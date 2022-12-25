bde33460c12905d2b3d62c6d9e7a5c468d6bebf4
BojanKrstevski <bojan.krstevski66@gmail.com>
2022-12-25 21:30:39 +0100

    test

--- Domasna 3/BojanKrstevski.github.io/src/Sidebar.js
+++
@@ -1,9 +0,0 @@
-const Sidebar = ({lng}) => {
-    return (<div className="sidebar">
-        {lng && <h1>Мапа на најблиските болници</h1>}
-        {!lng && <h1>Map that shows closes hospitals near you</h1>}
-
-    </div> );
-}
- 
-export default Sidebar;
